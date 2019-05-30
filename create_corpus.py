d2ae6959e877a9d091e12fff3c3c0740fcfff9c7
filipdbrsk <filip.dbrsk@gmail.com>
2019-05-30 12:00:25 +0200

    data analysis and better corpus

--- create_corpus.py
+++ create_corpus.py
@@ -1,13 +1,18 @@
-rom lxml import etree
+from lxml import etree
 import json
+import os
+import glob
 
 tree = etree.parse("input_data/nlwikinews-latest-pages-articles.xml")
 root = tree.getroot()
 
+for f in glob.glob('documents/*'):
+    os.remove(f)
+
 counter=1
 for x in root.findall('{http://www.mediawiki.org/xml/export-0.10/}page'):
     title=x.find('{http://www.mediawiki.org/xml/export-0.10/}title').text
-    if title.startswith('Categorie:') or title.startswith('MediaWiki:') or title.startswith('Wikinieuws:') or 'Nieuwsbrief Wikimedia Nederland' in title:
+    if title.startswith('H:') or title.startswith('WN:') or title.startswith('Sjabloon:') or title.startswith('Bestand:') or title.startswith('Help:') or title.startswith('Module:') or title.startswith('Categorie:') or title.startswith('MediaWiki:') or title.startswith('Wikinieuws:') or 'Nieuwsbrief Wikimedia Nederland' in title:
         continue
     text=x.find('{http://www.mediawiki.org/xml/export-0.10/}revision/{http://www.mediawiki.org/xml/export-0.10/}text').text
     if len(text)<100:
@@ -15,7 +20,7 @@ for x in root.findall('{http://www.mediawiki.org/xml/export-0.10/}page'):
 
     j={'title': title, 
        'body': text}    
-    
+
     with open('documents/wiki_%d.json' % counter, 'w') as outfile:
         json.dump(j, outfile)
     counter+=1
