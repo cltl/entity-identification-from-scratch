97ac99bf00c6fb895e3016f060c6a88a239b0fa7
sarnoult <sophie.arnoult@posteo.net>
2019-10-07 22:46:09 +0200

    adapts make_nif_corpus.py configuration

--- make_nif_corpus.py
+++ make_nif_corpus.py
@@ -1,12 +1,15 @@
 import load_utils
 import config
 
-raw_input_dir=config.raw_input_dir
-raw_input=raw_input_dir + '/leipzig_nl.ttl'
-max_docs=config.max_documents
+cfg = config.Config('cfg/nif_abstract_context_10.yml')
+cfg.setup_input()
 
-news_items = load_utils.load_article_from_nif_files(raw_input_dir, 
-                                       limit=max_docs or 1000000,
-                                       collection=config.corpus_name)
+# raw_input_dir=config.raw_input_dir
+# raw_input=raw_input_dir + '/leipzig_nl.ttl'
+# max_docs=config.max_documents
+
+news_items = load_utils.load_article_from_nif_files(cfg.raw_input_dir,
+                                                    limit=cfg.max_documents or 1000000,
+                                                    collection=cfg.corpus_name)
 
 load_utils.save_news_items('%s/documents.pkl' % config.data_dir, news_items)
