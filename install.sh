5715bc652c56b3fbb0d304ea15132da137fd2a5b
Filip Ilievski <filip.dbrsk@gmail.com>
2019-11-03 09:56:15 +0100

    minimal requirements.txt

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rm -f spacy_to_naf.py
-wget https://raw.githubusercontent.com/cltl/SpaCy-to-NAF/master/spacy_to_naf.py
+#rm -f spacy_to_naf.py
+#wget https://raw.githubusercontent.com/cltl/SpaCy-to-NAF/master/spacy_to_naf.py
 
 python -m spacy download nl_core_news_sm
 
